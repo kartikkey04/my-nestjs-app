391a1604b240571dcad4a320ee6826454229fecb
kartikey <kartikkey0414@gmail.com>
2025-04-17 11:34:28 +0530

    Added get user by id route

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Param, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
@@ -7,6 +7,12 @@ export class UsersController {
     getAllUsers(){
         const usersService = new UsersService();
         return usersService.getAllUsers();
+    }
+
+    @Get(':id')
+    getUserById(@Param('id') id: any){
+        const usersService = new UsersService();
+        return usersService.getUserById(+id);
     }
 
     @Post()
